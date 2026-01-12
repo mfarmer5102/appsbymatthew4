d20ccfcabcd24a801759f76cd4b0d131633a50ef
mfarmer5102 <48388789+mfarmer5102@users.noreply.github.com>
2026-01-11 21:02:39 -0500

    working on fixing aws secret consumption issues

--- backend/src/configuration/secrets.js
+++ backend/src/configuration/secrets.js
@@ -1,7 +1,7 @@
 import {SecretConfig} from '../_library/classes/secrets.js';
 
 const secret_config = new SecretConfig("us-east-1");
-secret_config.attach_secret('MONGO_INSTANCE_URL', 'prd-secrets');
-secret_config.attach_secret('APPSBYMATTHEW_ADMIN_CODE', 'prd-secrets');
+await secret_config.attach_secret('MONGO_INSTANCE_URL', 'prd-secrets');
+await secret_config.attach_secret('APPSBYMATTHEW_ADMIN_CODE', 'prd-secrets');
 
 export default secret_config;
